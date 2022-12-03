475c01d2accb28af93f851b02c636944f9dc09e2
thebrainfox <thebrainfoxytb44@gmail.com>
2022-12-03 04:22:30 +0100

    created gitignore + push antérieur failed

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ async function getOpenAiResponse(question) {
   const response = await openai.createCompletion({
     model: "text-davinci-002",
     prompt:
-      'Convert this text to a programmatic command:\n\nExample: Ask Constance if we need some bread.\n[action: `send msg`, target:`constance`, content:`Do we need some bread?`]\n\nExample: delete this channel.\n[action: `delete channel`, target:`current channel`, guild:`current guild`]\n\nExample: delete the channel test.\n[action: `delete channel`, target:`test`, guild:`current guild`]\n\nExample: delete the channel test on the guild something.\n[action: `delete channel`, target:`test`, guild:`something`]\n\nExample: delete the channel test on the server something.\n[action: `delete channel`, target:`test`, guild:`something`]\n\nExample: in one hour, delete the channel test.\n[action: `delete channel`, target:`test`, guild:`something`, await:`60`]\n\nExample: in one day ask thebrainfox for some help.\n[action: `send msg`, target:`thebrainfox`, await:`1440`]\n\nExample: create the channel test.\n[action: `create channel`, target:`test`, guild:`current guild`]\n\nExample: create the channel test on the guild something.\n[action: `create channel`, target:`test`, guild:`something`]\n\nExample: create the channel test on the guild something.\n[action: `create channel`, target:`test`, guild:`something`]\n\nExample: ban myself of this guild.\n[action: `ban`, target:`current user`, guild:`current guild`]\n\nExample: send an embed.\n[action: `send embed`, target:`current channel`, title:`this is the title`, content:`this is the content`]\n\nExample: send an embed with "name" as the title\n[action: `send embed`, target:`current channel`, title:`name`, content:`this is the content`]\n\nExample: send an embed with "blah blah blah" as the content\n[action: `send embed`, target:`current channel`, title:`this is the title`, content:`blah blah blah`]\n' +
+      'Convert this text to a programmatic command:\n\nExample: Ask Constance if we need some bread.\n[action: `send msg`, target:`constance`, content:`Do we need some bread?`]\n\nExample: delete this channel.\n[action: `delete channel`, target:`current channel`, guild:`current guild`]\n\nExample: delete the channel test.\n[action: `delete channel`, target:`test`, guild:`current guild`]\n\nExample: delete the channel test on the guild something.\n[action: `delete channel`, target:`test`, guild:`something`]\n\nExample: delete the channel test on the server something.\n[action: `delete channel`, target:`test`, guild:`something`]\n\nExample: in one hour, delete the channel test.\n[action: `delete channel`, target:`test`, guild:`something`, await:`60`]\n\nExample: in one day ask thebrainfox for some help.\n[action: `send msg`, target:`thebrainfox`, await:`1440`]\n\nExample: create the channel test.\n[action: `create channel`, target:`test`, guild:`current guild`]\n\nExample: create the channel test on the guild something.\n[action: `create channel`, target:`test`, guild:`something`]\n\nExample: create the channel test on the guild something.\n[action: `create channel`, target:`test`, guild:`something`]\n\nExample: ban myself of this guild.\n[action: `ban`, target:`current user`, guild:`current guild`]\n\nExample: send an embed.\n[action: `send embed`, target:`current channel`, title:`this is the title`, content:`this is the content`]\n\nExample: send an embed with "name" as the title\n[action: `send embed`, target:`current channel`, title:`name`, content:`this is the content`]\n\nExample: send an embed with "blah blah blah" as the content\n[action: `send embed`, target:`current channel`, title:`this is the title`, content:`blah blah blah`]\n\nExample: mute kosei\n[action: `mute`, target:`kosei`, channel:`current channel`, duration:`1440`]\n\nExample: mute litlepapaye 45min \n[action: `mute`, target:`litlepapaye`, channel:`current channel`, duration:`45`] \n\nmute thebrainfox \n[action:`mute`, target:`thebrainfox`, channel:`current channel`, duration: 1440]\n' +
       question +
-      ".",
+      ".\n",
     temperature: 0,
     max_tokens: 100,
     top_p: 1,
@@ -43,6 +43,10 @@ const { default: test } = require("node:test");
 const { text } = require("stream/consumers");
 const DB_settings = new Datastore({
   filename: "./db/settings.db",
+  autoload: true,
+});
+const DB_train = new Datastore({
+  filename: "./db/train.jsonl",
   autoload: true,
 });
 const blagues = new BlaguesAPI(
@@ -83,21 +87,34 @@ client.on("ready", async () => {
 
 client.on("messageCreate", async (msg) => {
   if (msg.author.bot) return;
-  let res = await getOpenAiResponse(msg.content);
+  if (!msg.content.startsWith("!")) return;
+  let txt = msg.content.replace("!", 1);
+  let res = await getOpenAiResponse(txt);
   if (
     res == "" ||
     res == "\n" ||
     res == "This is not a programmatic command."
   ) {
-    return msg.channel.send(
-      "sorry but i dont understand this. try to use basic commands because i didnt implemented all the discord features in this bot. you may also talk in english because the AI is soooo much more trained to recognize english meanings."
-    );
+    return msg.channel
+      .send(
+        "sorry but i dont understand this. try to use basic commands because i didnt implemented all the discord features in this bot. you may also talk in english because the AI is soooo much more trained to recognize english meanings."
+      )
+      .then((m) => {
+        setTimeout(() => {
+          m.delete();
+        }, 1000);
+      });
   }
   msg.channel.send(res);
+  msg.react('✅')
+  msg.react('❌')
 });
 
 client.on("error", (error) => {
   console.error(`client's WebSocket encountered a connection error: ${error}`);
 });
+console.log(process.env.DISCORD_TOKEN);
 
-client.login(process.env.DISCORD_TOKEN);
+client.login(
+  "MTAyMDA2ODY2OTA2MzE3NjIwMg.GiTioY.GoP4sTW-YiO2nutk0U-AxCkKqm2ZcG_GSFQa50"
+);
